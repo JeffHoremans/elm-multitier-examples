c39f31079a337dcb6747431acc13cb2f0101e80e
JeffHoremans <horemansjeff@gmail.com>
2017-02-03 08:00:20 +0100

    Applied changes

--- src/ChatDebug/Main.elm
+++ src/ChatDebug/Main.elm
@@ -7,7 +7,7 @@ import Chat.Main as Main
 
 -- MAIN
 
-program : MultitierProgram (Model Main.Model Main.Msg Main.ServerModel Main.ServerMsg) (ServerModel Main.ServerModel Main.ServerMsg Main.Model Main.Msg) (Msg Main.Msg) (ServerMsg Main.ServerMsg)
+program : MultitierProgram (Model Main.Model Main.Msg Main.ServerModel Main.ServerMsg) (ServerModel Main.ServerModel Main.ServerMsg Main.Model Main.Msg) (Msg Main.Model Main.Msg Main.ServerModel Main.ServerMsg) (ServerMsg Main.ServerMsg)
 program =
   Debugger.program
     { config = Main.config
